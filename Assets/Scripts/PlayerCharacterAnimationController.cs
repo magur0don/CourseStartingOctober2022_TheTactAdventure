340d609b5c50586caa350ef072a4307a9cb760f1
KotaShogo <>
2023-03-05 17:10:30 +0900

    physics logi change

--- Assets/Scripts/PlayerCharacterAnimationController.cs
+++ Assets/Scripts/PlayerCharacterAnimationController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerCharacterAnimationController : CharacterAnimationControllerBase
 {
@@ -30,15 +31,20 @@ public class PlayerCharacterAnimationController : CharacterAnimationControllerBa
         var pos = transform.position;
         pos.y += characterUnderPosYDiff;
         // Cast a ray straight down.
-        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.down,1f);
-        if (hit.collider != null && hit.collider.tag == "Ground")
-        {
-            isGround = true;
-        }
-        else
+        RaycastHit2D[] hits = Physics2D.RaycastAll(pos, Vector2.down,1f);
+        if (hits.Count() == 0)
         {
             isGround = false;
+            return;
         }
+        foreach (var hit in hits)
+        {
+            if (hit.collider.tag == "Ground")
+            {
+                isGround = true;
+            }
+        }
+
     }
 
     // Update is called once per frame
