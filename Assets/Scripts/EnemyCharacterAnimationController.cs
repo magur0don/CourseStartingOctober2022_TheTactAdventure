d750be5e3ef6c6a8a15a7aa2d1500201b2b1c49e
KotaShogo <>
2023-03-22 18:47:13 +0900

    敵のaiを強化

--- Assets/Scripts/EnemyCharacterAnimationController.cs
+++ Assets/Scripts/EnemyCharacterAnimationController.cs
@@ -11,6 +11,17 @@ public class EnemyCharacterAnimationController : CharacterAnimationControllerBas
 
     public EnemyPlayerSearch EnemyPlayerSearch;
 
+    [SerializeField]
+    private Vector3[] wayPoints = new Vector3[2];
+
+    private enum wayPoint {
+        Invalide =-1,
+        StartPos,
+        EndPos
+    }
+
+    private float waitSeconds = 2f;
+
     // Start is called before the first frame update
     protected override void Start()
     {
@@ -19,24 +30,72 @@ public class EnemyCharacterAnimationController : CharacterAnimationControllerBas
         enemyRigidbody2D = this.GetComponent<Rigidbody2D>();
 
         SetAnimation(Animation_Idle);
+        for (int i=0;i< wayPoints.Length; i++)
+        {
+            wayPoints[i] += this.transform.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!EnemyPlayerSearch.IsSearchPlayer) {
+        // StartPosまでの移動
+        // wayPointを超えたらflipXを変更する
+        if (this.transform.position.x < wayPoints[(int)wayPoint.StartPos].x)
+        {
+            waitSeconds -= Time.deltaTime;
 
-            SetAnimation(Animation_Idle);
-            return;
+            if (0f > waitSeconds)
+            {
+                if (characterSpriteRenderer.flipX)
+                {
+                    characterSpriteRenderer.flipX = false;
+                    waitSeconds = 2f;
+                }
+            }
         }
+        else if (this.transform.position.x > wayPoints[(int)wayPoint.EndPos].x)
+        {
+            waitSeconds -= Time.deltaTime;
+            if (0f > waitSeconds)
+            {
+                if (!characterSpriteRenderer.flipX)
+                {
+                    characterSpriteRenderer.flipX = true;
+                    waitSeconds = 2f;
+                }
+            }
+        }
+
+        // サーチエリアに入らなければ
+        if (!EnemyPlayerSearch.IsSearchPlayer)
+        {
+
+            if (characterSpriteRenderer.flipX && this.transform.position.x > wayPoints[(int)wayPoint.StartPos].x)
+            {
+                SetAnimation(Animation_Walk);
+                enemyRigidbody2D.velocity = Vector2.left * speedPower;
+                return;
+            }
+            else if (!characterSpriteRenderer.flipX && this.transform.position.x < wayPoints[(int)wayPoint.EndPos].x)
+            {
+                SetAnimation(Animation_Walk);
+                enemyRigidbody2D.velocity = Vector2.right * speedPower;
+                return;
+            }
+            else {
+                SetAnimation(Animation_Idle);
+                return;
+            }
+        }
+
+
         if (EnemyPlayerSearch.PlayerCharacterDistance < 0)
         {
-            //playerRigidbody2D.velocity = Vector2.right * speedPower;
             SetAnimation(Animation_Attack);
             characterSpriteRenderer.flipX = false;
         }
         else {
-            //playerRigidbody2D.velocity = Vector2.left * speedPower;
             SetAnimation(Animation_Attack);
             characterSpriteRenderer.flipX = true;
         }
